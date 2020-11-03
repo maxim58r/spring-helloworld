45d8d4544671c3ef3c1d9758540ccc904dd9d1e2
Maxim Serov <maxim58r@gmail.com>
2020-11-03 13:14:11 +0300

    2.1.2 done!

--- spring-helloworld-master/src/main/java/App.java
+++ spring-helloworld-master/src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.equals(bean));
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean.getName());
+        System.out.println(catBean.equals(catBean1));
     }
 }
